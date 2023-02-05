aff784ee5c6379d4df5c5ed8076d0c90c3ff9ef0
Terion27 <terion@mail.ru>
2023-02-05 23:41:50 +0300

    refactoring codes

--- src/main/java/quotes/config/BotConfig.java
+++ src/main/java/quotes/config/BotConfig.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
-
 @Getter
 @Slf4j
 @Configuration
